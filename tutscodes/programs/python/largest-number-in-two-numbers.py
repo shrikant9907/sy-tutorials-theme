cf64383b9e23ca181bb6d50f9695623a20c64fa7
Shrikant Dev <shrikant9907@gmail.com>
2020-07-12 17:09:03 +0530

    python programs

---
+++ tutscodes/programs/python/largest-number-in-two-numbers.py
@@ -0,0 +1,11 @@
+number1 = 40
+number2 = 80
+
+# Check the largest number
+if number1 > number2: # false
+    largestNumber = number1
+else:
+    largestNumber = number2
+
+# Display the result
+print('The largest number is '+ str(largestNumber))
---
+++ tutscodes/programs/python/odd-and-even-number.py
@@ -0,0 +1,10 @@
+number = 40
+
+# Check the Remainder
+if number % 2 == 0:
+    result = 'Even'
+else:
+    result = 'Odd'
+
+# Display the result
+print('The number '+ str(number) +' is '+ str(result))
